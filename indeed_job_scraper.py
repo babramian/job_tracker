f88548f582a8c4d6eac8e33cd970b941f5006b86
babramian <brianabramian@gmail.com>
2021-06-25 03:26:00 -0700

    created indeed scraper, currently gets links, titles, companies of job applications and whether they are remote or not, takes username and argument as terminal parameters.

--- indeed_job_scraper.py
+++ indeed_job_scraper.py
@@ -1,8 +1,64 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
 import pandas as pd
+import sys
+import time
 
-driver = webdriver.Chrome('Chrome Driver/chromedriver')
+# Using fake user agents
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent={userAgent}')
+driver = webdriver.Chrome(chrome_options=options, executable_path='Chrome Driver/chromedriver')# Launch Chrome web driver
 
+# get username & password from terminal launch command
+username = sys.argv[1]
+password = sys.argv[2]
+
+
+
+# connect to applied jobs page
 driver.get('https://myjobs.indeed.com/applied?hl=en&co=US&from=_atweb_gnav-jobsearch--jasx')
 
+# login
+inputElement = driver.find_element_by_id("login-email-input")
+inputElement.send_keys(username)
+inputElement = driver.find_element_by_id("login-password-input")
+inputElement.send_keys(password)
+inputElement.submit()
+print("Press any key when you've completed the captcha")
+input()
+
+# find 
+appCards = driver.find_elements_by_xpath('//div[contains(@class, "atw-AppCard")]')
+locations = driver.find_elements_by_xpath('//div[contains(@class, "atw-JobInfo-companyLocation")]')
+count = 0
+dataIds = []
+links = []
+job_titles = []
+companys = []
+remote = []
+for appCard in appCards:
+	dataid = appCard.get_attribute('data-id')
+	
+	if dataid != None:
+		currentCard = driver.find_element_by_xpath(f'//div[contains(@data-id, "{dataid}")]')
+		dataIds.append(dataid)
+		links.append('https://www.indeed.com/viewjob?jk=' + dataid + '&hl=en')
+		hrefCard = currentCard.find_element_by_xpath(f'//a[contains(@href, "https://www.indeed.com/viewjob?jk={dataid}&hl=en")]')
+		job_titles.append(hrefCard.text.split("\n", 1)[0])
+
+
+for location in locations:
+	companys.append(location.text.split("\n", 1)[0])
+	remote.append(1) if ("Remote" in location.text or "REMOTE" in location.text or "Remote" in job_titles[count] or "REMOTE" in job_titles[count]) else remote.append(0)
+	print(job_titles[count])
+	count += 1
+print(companys)
+print(remote)
